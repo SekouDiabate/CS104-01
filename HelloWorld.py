8cd272a28f856ddfa2597cc1292ccde6917ebbac
SekouDiabate <114018454+SekouDiabate@users.noreply.github.com>
2022-10-19 15:00:02 -0400

    Add files via upload
    HelloWorld Prt1

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================ RESTART: Shell ================================
+>>> 
+======== RESTART: /Users/sekoudiabate/Documents/Hello-World to Github.py =======
+hello my name is insert your first name
