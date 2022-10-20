7f7c24b693a3fa84d2dc360a3a343dfa314e137b
SekouDiabate <114018454+SekouDiabate@users.noreply.github.com>
2022-10-19 22:57:30 -0400

    Condition

---
+++ Condition3.py
@@ -0,0 +1,13 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /Users/sekoudiabate/Desktop/CS 101/Conditions .py ==========
+Please enter the current temperature or -1 to end: 78
+Short sleeves are fine
+Please enter the current temperature or -1 to end: 99
+Wear Shorts
+Please enter the current temperature or -1 to end: 62
+Wear a jacket
+Please enter the current temperature or -1 to end: 32
+Stay inside
+Please enter the current temperature or -1 to end: -1
