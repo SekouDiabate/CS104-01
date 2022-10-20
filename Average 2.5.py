202c6feefd3b2f72b55e79051cdd0c1fb3d40e5c
SekouDiabate <114018454+SekouDiabate@users.noreply.github.com>
2022-10-19 22:49:27 -0400

    Average

---
+++ Average 2.5.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: /Users/sekoudiabate/Desktop/CS 101/Average.py ============
+How many test scores would you like to average?3
+Enter test score: 60
+Enter test score: 70
+Enter test score: 80
+The average for the testscores you entered is:  70.0
